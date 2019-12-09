fbef9aad11390b4c0e3da21a75356b7b9b960424
rolandoquiroz <rolandoquiroz@gmail.com>
2019-12-09 21:23:48 +0000

    [Re-Add] 0-create_a_file.pp file again

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Using Puppet, install puppet-lint.
 file { '/tmp/holberton':
   owner   => 'www-data',
-  group	  => 'www-data',
-  mode	  => '0744',
+  group    => 'www-data',
+  mode    => '0744',
   content => 'I love Puppet'
 }
