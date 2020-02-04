a14663d322a8555feb35a882899ea36f0dc70110
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-04 00:06:28 -0500

    [Feature] Task 1: 1-export_to_CSV.py Python script file added.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,16 +19,15 @@ if __name__ == "__main__":
     url_user = ("https://jsonplaceholder.typicode.com/users/{}".
                 format(employee_ID))
     response_user = requests.get(url_user)
+    user_dict = response_user.json()
 
     url_todo = ("https://jsonplaceholder.typicode.com/todos?userID={}".
                 format(employee_ID))
     response_todo = requests.get(url_todo)
-
-    user_dict = response_user.json()
     todo_dict = response_todo.json()
 
     with open("{}.csv".format(employee_ID), 'a+') as csvf:
         f = csv.writer(csvf, delimiter=',', quoting=csv.QUOTE_ALL)
         for task in todo_dict:
-            f.writerow([employee_ID, user_dict['username'],
-                        task.get('completed'), task['title']])
+            f.writerow([employee_ID, user_dict.get('username'),
+                        task.get('completed'), task.get('title')])
