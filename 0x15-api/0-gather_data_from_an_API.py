543a6cee44eb85b8e39fe8418aaf750facf2c53e
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-03 22:21:46 -0500

    [Fix] Task 0: 0-gather_data_from_an_API.py Python script file added exception requirement

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,39 +7,35 @@ returns information about his/her TODO list progress.
 
 if __name__ == '__main__':
     '''
-    This won't be run when imported
+    This won't be run when imported """
     '''
-    from sys import argv
     import requests
+    from sys import argv
 
     employee_ID = argv[1]
 
     url_user = ("https://jsonplaceholder.typicode.com/users/{}".
                 format(employee_ID))
     response_user = requests.get(url_user)
-    
+    user_dict = response_user.json()
+
     url_todo = ("https://jsonplaceholder.typicode.com/todos?userId={}".
                 format(employee_ID))
     response_todo = requests.get(url_todo)
+    todo_dict = response_todo.json()
 
-    try:
-        user_dict = response_user.json()
-        todo_dict = response_todo.json()
-        tasks_done = []
+    tasks_done = []
 
-        for task in todo_dict:
-                if task.get('completed') is True:
-                        tasks_done.append(task.get('title'))
+    for task in todo_dict:
+        if task.get('completed'):
+            tasks_done.append(task.get('title'))
 
-        total_tasks = len(todo_dict)
+    total_tasks = len(todo_dict)
 
-        name = user_dict.get('name')
+    name = user_dict.get('name')
 
-        print("Employee {} is done with tasks({}/{}):".
-              format(name, len(tasks_done), total_tasks))
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, len(tasks_done), total_tasks))
 
-        for task in tasks_done:
-                print("\t {}".format(task))
-
-    except:
-        pass
+    for task in tasks_done:
+        print("\t {}".format(task))
