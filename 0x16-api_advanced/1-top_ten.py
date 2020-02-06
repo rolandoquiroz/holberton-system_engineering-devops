3b7d40e1b94ab43dd8bcaba4b521bc1c6f1f819a
rolandoquiroz <rolandoquiroz_inc@hotmail.com>
2020-02-06 14:52:46 -0500

    [Fix] Task 1: Top ten pep8 checked

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 import requests
 """
-MethodModule that queries the Reddit API and returns the number
-of subscribers (not active users, total subscribers) for
-a given subreddit.
+Module that queries the Reddit API and prints the titles of
+the first 10 hot posts listed for a given subreddit.
 """
 
 
 def top_ten(subreddit):
     """
-    This method queries the Reddit API and returns the number
-    of subscribers
-    If an invalid subreddit is given, the function should return 0.
+    This method queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
     """
-    url = "https://api.reddit.com/r/{}/?sort=hot&limit=10".format(subreddit)
+    url = "https://api.reddit.com/r/{}/?limit=10".format(subreddit)
     response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     if response.status_code is 200:
         response_json = response.json()
