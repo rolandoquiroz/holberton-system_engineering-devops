49bbe749dfbeb37ee6d970308ce66d61020ae0f1
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-28 00:49:42 -0500

    Adds Task 5 answer file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # 0. Sky is the limit, let's bring that limit higher
 
 exec { 'fix wp-settings.php file':
-  command => "sed -i 's|15|5000|g' /etc/default/nginx",
-  path    => ['/bin']
+  command => "sed -i 's|15|4096|g' /etc/default/nginx",
+  path    => ['/usr/bin/env']
 }
 
 exec { 'nginx restart':
   command => 'service nginx restart',
-  path    => ['/usr/bin']
+  path    => ['/usr/bin/env']
 }
